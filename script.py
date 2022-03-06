2743c178b4bdddcb88c63f3d3815123ee7997179
soymadip <84225810+soymadip@users.noreply.github.com>
2022-03-06 16:37:42 +0530

    :100:

---
+++ script.py
@@ -0,0 +1,30 @@
+class script(object):
+
+
+    START_MSG = """ <b>"ℍ𝔼𝕃𝕃𝕆 {}\n\n 𝕀 𝔸𝕄 <a href=https://t.me/{}>{}</a>\n 𝕀 𝔻𝔼𝕃𝕀𝕍𝕀𝔼ℝ ℝ𝔼ℚ𝕌𝔼𝕊𝕋𝔼𝔻 𝕄𝔼𝔻𝕀𝔸 𝕋𝕆 ℙ𝔼𝕆ℙ𝕃𝔼."""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+<i>    
+* In channel, make texts </b>Link Text</b>!
+
+* Add bot to group with admin rights.
+
+* Once deployed and bot is started, send a message to channel (must do step)
+
+* Bot will work now :)
+
+No need add each filter again!
+Bot will automatically search for your message and give links to that!
+</i>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
+    
+⭕️<b>Creater :</b> Don't know :)
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>  
+
+"""

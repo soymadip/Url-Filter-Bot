c059ffdb82a008b4c9f6e28f15de5913f182fe12
soymadip <84225810+soymadip@users.noreply.github.com>
2022-03-12 14:32:55 +0530

    :100:

--- plugins/filters.py
+++ plugins/filters.py
@@ -18,7 +18,7 @@ from pyrogram.types import (
 
 from bot import Bot
 from script import script
-from config import MAINCHANNEL_ID
+from config import MAINCHANNEL_ID, ADMINS
 
 BUTTONS = {}
  
@@ -207,3 +207,17 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 def split_list(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]  
+
+@Client.on_message(filters.text & filters.private & filters.incoming) #PM guide module
+async def filter(client, message):
+    if ADMINS and message.from_user and message.from_user.id in ADMINS:
+        return True
+    if message.text.startswith("/"):
+        return 
+    if 2 < len(message.text) < 50:
+        btn = [
+        [
+            InlineKeyboardButton('⚡️ ℂ𝕀ℕ𝔼𝕄𝔸 ℍ𝕌𝔹 ⚡️', url=f'https://t.me/cinemaforyou07')
+        ]
+        ]
+        await client.send_message(chat_id=message.from_user.id, text='🔰𝗡𝗢𝗧𝗜𝗖𝗘🔰\n\nDo not request here😡\nThis chat is only for <u>movie delevery</u>.\n\n<b>Tell your query in CINEMA HUB group👇🏻</b>', reply_markup=InlineKeyboardMarkup(btn))

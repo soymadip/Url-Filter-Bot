89443a956314bbf7cb16776a2ee57c83a3a82c84
soumadip <soumadip377@gmail.com>
2022-04-04 21:36:15 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 class script(object):
 
 
-    START_MSG = """ <b>"ℍ𝔼𝕃𝕃𝕆 {}\n\n 𝕀 𝔸𝕄 <a href=https://t.me/{}>{}</a>\n 𝕀 𝔻𝔼𝕃𝕀𝕍𝕀𝔼ℝ ℝ𝔼ℚ𝕌𝔼𝕊𝕋𝔼𝔻 𝕄𝔼𝔻𝕀𝔸 𝕋𝕆 ℙ𝔼𝕆ℙ𝕃𝔼."""
+    START_MSG = """ <b>Hi {}
+You can call this as an Auto Filter Bot if you like :D
+Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
+So no need to add filters for your files or movies from now on!
+For more click <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>

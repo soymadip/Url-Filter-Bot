4aed695102275758f4442dc1ac60b836f9d828a4
soumadip <soumadip377@gmail.com>
2022-04-04 21:47:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,9 +2,7 @@ class script(object):
 
 
     START_MSG = """ <b>Hi {}
-You can call this as an Auto Filter Bot if you like :D
-Bot simply search for the files from provided channel according to given query and gives link to those files as buttons!
-So no need to add filters for your files or movies from now on!
+I simply search for the files from provided channel according to given query and gives link to those files as buttons!
 For more click <i>help</i></b>"""
 
 

243f9bc1237c275a3999f1750ed09fbf9bdf7fba
soumadip <soumadip377@gmail.com>
2022-04-04 21:17:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,28 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from script import script
 
 
-@Client.on_message(filters.command(["start"]) & filters.private)
-async def start(client, message):
-    try:
-        await message.reply_text(
-            text=script.START_MSG.format(message.from_user.mention),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("HELP", callback_data="help_data"),
-                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "⭕️ ADMIN ⭕️", url="https://t.me/anonymous7205")
-                    ]
-                ]
-            ),
-            reply_to_message_id=message.message_id
-        )
-    except:
-        pass
+
 
 @Client.on_message(filters.command(["help"]) & filters.private)
 async def help(client, message):

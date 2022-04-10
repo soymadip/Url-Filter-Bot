cb4369a5e52bd4e13ee774be294fddbe55709756
soumadip <soumadip377@gmail.com>
2022-04-10 15:48:03 +0530

    Create Guide.py

---
+++ plugins/Guide.py
@@ -0,0 +1,28 @@
+
+
+import re
+import pyrogram
+import asyncio
+
+from pyrogram import (
+    filters,
+    Client
+)
+
+from pyrogram.types import (
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup, 
+    Message,
+    CallbackQuery
+)
+
+from bot import Bot
+from script import script
+from config import MAINCHANNEL_ID, ADMINS, SHIFT_CHANNEL
+
+
+
+@Client.on_message(filters.incoming & filters.text)
+async def filter(client: Bot, message: Message):
+    if message.chat.id = SHIFT_CHANNEL:
+        await client.send_message(SHIFT_CHANNEL,f'done bro')
